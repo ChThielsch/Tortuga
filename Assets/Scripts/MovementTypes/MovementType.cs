f9571182042b32b81deeb71569a181f6c9b432ca
Christian <thielsch@buntspecht.com>
2023-07-25 17:10:33 +0200

    Updated turtle swim movement

--- Assets/Scripts/MovementTypes/MovementType.cs
+++ Assets/Scripts/MovementTypes/MovementType.cs
@@ -18,64 +18,45 @@ public class MovementType : ScriptableObject
     }
 
     [Divider("Force")]
-    public float maxForce = 50f;
     public AnimationCurve forceCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
-    public float timeToReachMaxForce = 2f;
+    public float maxSpeed = 10f;
+    public float forceBuildUpSpeed = 1f; // New variable for controlling force build-up speed
 
     private float m_currentForce = 0f;
-    private float m_elapsedTime = 0f;
+    private float m_accumulatedForce = 0f;
 
-    public float maxSpeed = 10f;
-
-    public float horizontalMultiplier = 1f;
-    public float verticalMultiplier = 1f;
-
-    [Range(0f, 1f)] public float weight = 0.5f; // New weight variable
+    [Range(0f, 1f)]
+    public float weight = 0.5f;
 
     public void ApplyConstantForce(float _swimInput, Vector2 _movementInput, Rigidbody _rigidbody)
     {
         if (_swimInput == 0f)
         {
-            ResetForce();
+            m_accumulatedForce = 0f;
             return;
         }
 
         _movementInput *= -1;
 
-        float normalizedSwimInput = _swimInput;
-        float modifiedMaxForce = maxForce * normalizedSwimInput;
-        float modifiedTimeToReachMaxForce = timeToReachMaxForce * (1f / normalizedSwimInput);
+        float maxForce = maxSpeed * _swimInput;
+        float curveValue = forceCurve.Evaluate(_swimInput);
+        m_currentForce = Mathf.Lerp(0f, maxForce, curveValue);
 
-        m_elapsedTime += Time.deltaTime;
-
-        // Evaluate the AnimationCurve at the current time to get the force increment factor
-        float curveValue = forceCurve.Evaluate(m_elapsedTime / modifiedTimeToReachMaxForce);
-
-        // Calculate the current force based on the modified max force and the curve value
-        m_currentForce = Mathf.Lerp(0f, modifiedMaxForce, curveValue);
+        // Increment the accumulated force over time with the build-up speed control
+        m_accumulatedForce = Mathf.MoveTowards(m_accumulatedForce, m_currentForce, forceBuildUpSpeed * Time.deltaTime);
 
         Vector3 forwardDirection = _rigidbody.transform.forward;
         Vector3 rightDirection = _rigidbody.transform.right;
         Vector3 upDirection = _rigidbody.transform.up;
 
-        // Calculate the force based on the input and the rigidbody's forward direction
         Vector3 inputForce = _movementInput.x * rightDirection + _movementInput.y * upDirection;
         Vector3 forwardForce = forwardDirection;
 
-        // Blend the two forces based on the weight
         Vector3 blendedForce = Vector3.Lerp(inputForce, forwardForce, weight).normalized;
 
-        Vector3 forceVector = m_currentForce * blendedForce;
+        Vector3 forceVector = m_accumulatedForce * blendedForce;
 
         _rigidbody.AddForce(forceVector, ForceMode.Force);
-
-        // Limit the Rigidbody's velocity to the specified maxSpeed
         _rigidbody.velocity = Vector3.ClampMagnitude(_rigidbody.velocity, maxSpeed);
-    }
-
-    public void ResetForce()
-    {
-        m_currentForce = 0f;
-        m_elapsedTime = 0f;
     }
 }
